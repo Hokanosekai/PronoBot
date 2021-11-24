b0a078067525c614602b90285fb60ca1af091ca1
Hokanosekai <69720899+Hokanosekai@users.noreply.github.com>
2021-11-24 21:54:05 +0100

    Update log.js

--- util/log.js
+++ util/log.js
@@ -4,7 +4,7 @@ module.exports = (action) => {
     const date = require('date-and-time')
     const now = new Date()
     const value = date.format(now,'YYYY/MM/DD HH:mm:ss');
-    fs.appendFile(`${process.env.BOT_PATH}/logs.txt`, (value+action+'\n'), function (err) {
+    fs.appendFile(`${process.cwd()}/logs.txt`, (value+action+'\n'), function (err) {
         if (err) {
             return console.log(err);
         }
